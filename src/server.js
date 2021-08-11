8e01078923371b9f3d2d882f148251f5926ef765
khkg12 <khkg12@naver.com>
2021-08-11 13:48:41 +0900

    wetube 코딩

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
